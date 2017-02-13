9f033437efaa22fb40fe191afea7e11b45cdcb92
jiawei <jaewei.j.w@gmail.com>
2017-02-14 00:45:49 +0800

    Remove calendar import from dateApp. Switch to date class

--- src/sg/edu/nus/comp/cs4218/impl/app/DateApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/DateApplication.java
@@ -5,16 +5,17 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Locale;
 
 import sg.edu.nus.comp.cs4218.app.Date;
-import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
-import sg.edu.nus.comp.cs4218.exception.CatException;
 import sg.edu.nus.comp.cs4218.exception.DateException;
 
 public class DateApplication implements Date {
+	private java.util.Date currentDate;
 
+	/**
+	 * Runs application with specified input data and specified output stream.
+	 */
 	@Override
 	public void run(String[] args, InputStream stdin, OutputStream stdout) throws DateException {
 		// TODO Auto-generated method stub
@@ -29,16 +30,12 @@ public class DateApplication implements Date {
 		if (stdout == null) {
 			throw new DateException("No output stream provided");
 		}
-		if (args == null || args.length == 0 || stdin == null) {
+		if (stdin == null) {
 			// is the checking of stdin == null necessary?
-//			if (stdin == null) {
+			// Will there be a case InputStream will not be passed?
 			throw new DateException("No input stream provided");
-//			}
 		}
-		// Who checks if the command provided is in the right format?
-		// If dateapp checks it, remember to check for case insensitivity
-		
-		if (args.length == 0 && args[0].equalsIgnoreCase("date")) {
+		if (args.length == 1 && args[0].equalsIgnoreCase("date")) {
 			String currentDate = printCurrentDate(args[0]);
 			PrintWriter writer = new PrintWriter(stdout);
 			writer.println(currentDate);
@@ -48,14 +45,20 @@ public class DateApplication implements Date {
 		}
 	}
 	
+	/**
+	 * Print the current date and time
+	 * @param args String containing command
+	 */
 	@Override
 	public String printCurrentDate(String args) {
 		// TODO Auto-generated method stub
 		// test if locale.english gives the right ans
 		DateFormat dateformat = new SimpleDateFormat("EEE MMM d HH:mm:ss z yyyy", 
 													Locale.ENGLISH);
-		Calendar calobj = Calendar.getInstance();
-		return dateformat.format(calobj.getTime());
+
+		currentDate = new java.util.Date();
+	    String strDate = dateformat.format(currentDate);
+		return strDate;
 	}
 
 }
