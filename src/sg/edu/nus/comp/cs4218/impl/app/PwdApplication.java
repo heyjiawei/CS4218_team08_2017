0c870c9662e0a2ac4dc04ebde26cf03f60709694
Tham Zheng Yi <thamzhengyi@gmail.com>
2017-02-15 19:20:25 +0800

    Add and implement PwdApplication.

---
+++ src/sg/edu/nus/comp/cs4218/impl/app/PwdApplication.java
@@ -0,0 +1,42 @@
+package sg.edu.nus.comp.cs4218.impl.app;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import sg.edu.nus.comp.cs4218.Application;
+import sg.edu.nus.comp.cs4218.exception.PwdException;
+
+/**
+ * The pwd command reports the current working directory followed by a newline.
+ * 
+ * <p>
+ * <b>Command format:</b> <code>pwd</code>
+ * </p>
+ */
+public class PwdApplication implements Application {
+
+	/**
+	 * Runs the pwd application.
+	 * 
+	 * @param args
+	 *            Array of arguments for the application, not used.
+	 * @param stdin
+	 *            An InputStream, not used.
+	 * @param stdout
+	 *            An OutputStream. The output of the command is written to this
+	 *            OutputStream.
+	 * 
+	 * @throws PwdException
+	 *             If an I/O exception occurs.
+	 */
+	public void run(String[] args, InputStream stdin, OutputStream stdout)
+			throws PwdException {
+		try {
+			String currentWorkingDirectoryString = System.getProperty("user.dir");
+			stdout.write(currentWorkingDirectoryString.getBytes());
+		} catch (IOException e) {
+			throw new PwdException("IOException");
+		}
+	}
+}
