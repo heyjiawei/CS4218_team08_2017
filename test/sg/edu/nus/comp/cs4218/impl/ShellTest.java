4cf3156da0d3c00dbdce6d9eb8f36318862d43eb
Zhu Liang <zhuliangg11@gmail.com>
2017-02-21 14:10:00 +0800

    add command substitution test

--- test/sg/edu/nus/comp/cs4218/impl/ShellTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/ShellTest.java
@@ -2,22 +2,61 @@ package sg.edu.nus.comp.cs4218.impl;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.ByteArrayOutputStream;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import sg.edu.nus.comp.cs4218.Shell;
 import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
 import sg.edu.nus.comp.cs4218.exception.ShellException;
 
 public class ShellTest {
 
-	ShellImpl shell;
+	Shell shell;
 	String output;
+	ByteArrayOutputStream outputStream;
+	String lineSeparator = System.getProperty("line.separator");
 
 	@Before
 	public void setUp() throws Exception {
 		shell = new ShellImpl();
+		outputStream = new ByteArrayOutputStream();
 	}
-
+	
+	@Test
+	public void testBasicShell() throws AbstractApplicationException, ShellException {
+		String cmd = "echo cat";
+		
+		shell.parseAndEvaluate(cmd, outputStream);
+		
+		output = outputStream.toString();
+		
+		assertEquals("cat" + lineSeparator, output);
+	}
+	
+	@Test
+	public void testCommandSubstitution() throws AbstractApplicationException, ShellException {
+		String cmd = "echo `echo cat`";
+		
+		shell.parseAndEvaluate(cmd, outputStream);
+		
+		output = outputStream.toString();
+		
+		assertEquals("cat" + lineSeparator, output);
+	}
+	
+	@Test
+	public void testCommandSubstitutionWithPipe() throws AbstractApplicationException, ShellException {
+		String cmd = "echo `echo cat | wc -m`";
+		
+		shell.parseAndEvaluate(cmd, outputStream);
+		
+		output = outputStream.toString();
+		
+		assertEquals("4" + lineSeparator, output);
+	}
+	
 	@Test
 	public void testPipeTwoCommands() throws AbstractApplicationException, ShellException {
 		String cmd = "echo \"cd\" | wc";
