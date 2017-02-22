7602528f1f7e1734a72fb8d9ae499cc4ca8a16d6
Zhu Liang <zhuliangg11@gmail.com>
2017-02-22 13:20:41 +0800

    test command substitution using interface method

--- test/sg/edu/nus/comp/cs4218/impl/ShellTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/ShellTest.java
@@ -32,80 +32,59 @@ public class ShellTest {
 	@Test
 	public void testBasicShell() throws AbstractApplicationException, ShellException {
 		String cmd = "echo cat";
-		
 		shell.parseAndEvaluate(cmd, outputStream);
-		
 		output = outputStream.toString();
-		
 		assertEquals("cat" + newLine, output);
 	}
 	
 	@Test
 	public void testCommandSubstitution() throws AbstractApplicationException, ShellException {
 		String cmd = "echo `echo cat`";
-		
-		shell.parseAndEvaluate(cmd, outputStream);
-		
-		output = outputStream.toString();
-		
+		output = shell.performCommandSubstitution(cmd);
 		assertEquals("cat" + newLine, output);
 	}
 	
 	@Test
 	public void testInvalidCommandSubstitution() throws AbstractApplicationException, ShellException {
 		String cmd = "echo `echo `echo cat` cat`";
-		
-		thrown.expect(ShellException.class);
-		thrown.expectMessage(ShellImpl.EXP_SYNTAX);
-		
-		shell.parseAndEvaluate(cmd, outputStream);
+		output = shell.performCommandSubstitution(cmd);
+		assertEquals("shell: Invalid syntax encountered near echo cat` cat` ", output);
 		
 	}
 	
 	@Test
 	public void testCommandSubstitutionWithPipe() throws AbstractApplicationException, ShellException {
 		String cmd = "echo `echo cat | wc -m`";
-		
 		shell.parseAndEvaluate(cmd, outputStream);
-		
 		output = outputStream.toString();
-		
 		assertEquals("4" + newLine, output);
 	}
 	
 	@Test
 	public void testPipeTwoCommands() throws AbstractApplicationException, ShellException {
 		String cmd = "echo \"cd\" | wc";
-
 		output = shell.pipeTwoCommands(cmd);
-
 		assertEquals("3 1 1" + newLine, output);
 	}
 
 	@Test
 	public void testPipeMultipleCommands() throws AbstractApplicationException, ShellException {
 		String cmd = "echo \"cd\" | head | tail";
-
 		output = shell.pipeMultipleCommands(cmd);
-
 		assertEquals("cd", output);
 	}
 
 	@Test
 	public void testMultiplePipeOutput() throws AbstractApplicationException, ShellException {
 		String cmd = "echo hi | echo bye | echo yes";
-
 		output = shell.pipeMultipleCommands(cmd);
-
 		assertEquals("yes" + newLine, output);
 	}
 
 	@Test
 	public void testPipeWithException() throws AbstractApplicationException, ShellException {
 		String cmd = "head '   ' | echo unreachable";
-
 		output = shell.pipeWithException(cmd);
-
 		assertEquals("head: Could not read file", output);
 	}
 
