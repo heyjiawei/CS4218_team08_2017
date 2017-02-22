1793a7a62322e01076257a78a82f2f380103e1e5
Wu Di <justwudi@gmail.com>
2017-02-22 12:46:49 +0800

    Test the interface methods for redirection

--- test/sg/edu/nus/comp/cs4218/impl/ShellIORedirectionTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/ShellIORedirectionTest.java
@@ -1,6 +1,7 @@
 package sg.edu.nus.comp.cs4218.impl;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -12,8 +13,10 @@ import java.nio.file.Paths;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
+import org.junit.rules.ExpectedException;
 import sg.edu.nus.comp.cs4218.Shell;
 import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
 import sg.edu.nus.comp.cs4218.exception.ShellException;
@@ -22,9 +25,9 @@ public class ShellIORedirectionTest {
 	ByteArrayOutputStream outputStream;
 	private Shell shell;
 	private String output;
-	
+
 	private static final String NEW_LINE = System.getProperty("line.separator");
-	
+
 	private static final String TEST_STRING = "test";
 	private static final String TEST_FILE_PATH = "test_ioredirection/";
 	private static final String TEST_FILE_INPUT = "test_ioredirection/input.txt";
@@ -34,7 +37,10 @@ public class ShellIORedirectionTest {
 		byte[] encoded = Files.readAllBytes(Paths.get(path));
 		return new String(encoded, encoding);
 	}
-	
+
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
 	@Before
 	public void setUp() throws Exception {
 		shell = new ShellImpl();
@@ -45,7 +51,7 @@ public class ShellIORedirectionTest {
 		    out.print(TEST_STRING + NEW_LINE);
 		}
 	}
-	
+
 	@After
 	public void tearDown() throws Exception {
 		File tempOutputFile = new File(TEST_FILE_OUTPUT);
@@ -55,37 +61,81 @@ public class ShellIORedirectionTest {
 		File tempFileDir = new File(TEST_FILE_PATH);
 		tempFileDir.delete();
 	}
-	
+
 	@Test
-	public void testShellIORedirectionInputOutput() throws AbstractApplicationException, ShellException, IOException {
+	public void testRedirectInputOutput() throws AbstractApplicationException, ShellException, IOException {
 		String cmd = "cat"  + " < " + TEST_FILE_INPUT + " > " + TEST_FILE_OUTPUT;
-		
+
 		shell.parseAndEvaluate(cmd, outputStream);
-		
+
 		output = readFile(TEST_FILE_OUTPUT, Charset.defaultCharset());
-		
+
 		assertEquals(TEST_STRING + NEW_LINE, output);
 	}
-	
+
 	@Test
-	public void testShellIORedirectionOutput() throws AbstractApplicationException, ShellException, IOException {
+	public void testRedirectOutput() throws AbstractApplicationException, ShellException, IOException {
 		String cmd = "echo " + TEST_STRING  + " > " + TEST_FILE_OUTPUT;
-		
-		shell.parseAndEvaluate(cmd, outputStream);
-		
+
+		shell.redirectOutput(cmd);
+
 		output = readFile(TEST_FILE_OUTPUT, Charset.defaultCharset());
-		
+
 		assertEquals(TEST_STRING + NEW_LINE, output);
 	}
-	
+
 	@Test
-	public void testShellIORedirectionInput() throws AbstractApplicationException, ShellException {
+	public void testRedirectInput() throws AbstractApplicationException, ShellException {
 		String cmd = "cat"  + " < " + TEST_FILE_INPUT;
-		
-		shell.parseAndEvaluate(cmd, outputStream);
-		
-		output = outputStream.toString();
-		
+
+		output = shell.redirectInput(cmd);
+
 		assertEquals(TEST_STRING + NEW_LINE, output);
 	}
+
+	@Test
+	public void testRedirectInputWithNoFile() throws AbstractApplicationException, ShellException {
+		String cmd = "cat"  + " < ";
+
+		output = shell.redirectInputWithNoFile(cmd);
+
+		assertEquals("shell: Invalid syntax encountered near <", output);
+	}
+
+	@Test
+	public void testRedirectOutputWithNoFile() throws AbstractApplicationException, ShellException {
+		String cmd = "echo"  + " > ";
+
+		output = shell.redirectOutputWithNoFile(cmd);
+
+		assertEquals("shell: Invalid syntax encountered near >", output);
+	}
+
+	@Test
+	public void testRedirectInputWithException() throws AbstractApplicationException, ShellException {
+		String cmd = "cat '/  /'"  + " < " + TEST_FILE_INPUT;
+
+		output = shell.redirectInputWithException(cmd);
+
+		assertEquals("cat: Could not read file", output);
+	}
+
+	@Test
+	public void testRedirectOutputWithException() throws AbstractApplicationException, ShellException {
+		String cmd = "cat '/  /'"  + " > " + TEST_FILE_OUTPUT;
+
+		output = shell.redirectOutputWithException(cmd);
+
+		assertEquals("cat: Could not read file", output);
+	}
+
+	@Test
+	public void testRedirectOutputWithExceptionFileNotCreated() throws AbstractApplicationException, ShellException {
+		String cmd = "cat '/  /'"  + " > " + TEST_FILE_OUTPUT;
+
+		output = shell.redirectOutputWithException(cmd);
+
+		assertTrue(new File(TEST_FILE_OUTPUT).exists());
+	}
+
 }
